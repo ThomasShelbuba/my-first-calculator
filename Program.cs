fd85cf8a65b6835f27281cae27c0ea953b8f26a8
Bohdan <msugenius@gmail.com>
2023-01-29 14:59:34 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PrideAndPrejudice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Calculator");
+            System.Console.WriteLine();
+            System.Console.WriteLine("+");
+            System.Console.WriteLine("-");
+            System.Console.WriteLine("/");
+            System.Console.WriteLine("*");
+            string Negr = Console.ReadLine();
+            System.Console.WriteLine("Vvedite chislo1");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Vvedite chislo2");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            if (Negr == "+")
+            {
+                System.Console.WriteLine(numberOne + numberTwo);
+            }
+            if (Negr == "-")
+            {
+                System.Console.WriteLine(numberOne - numberTwo);
+            }
+            if (Negr == "/")
+            {
+                System.Console.WriteLine(numberOne / numberTwo);
+            }
+            if (Negr == "*")
+            {
+                System.Console.WriteLine(numberOne * numberTwo);
+            }
+            Console.ReadLine();
+        }
+    }
+}
